6634b1357769cad1496643ac5aeecd0a1aecb16b
krederik <krederik@gmail.com>
2018-09-11 10:42:18 +0200

    sdfkj

---
+++ src/main/java/clbo/demo/movieController.java
@@ -0,0 +1,13 @@
+package clbo.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class movieController {
+
+    @GetMapping("/")
+    public String index(){
+            return "index";
+    }
+}
